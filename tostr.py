6710598dff3f647fa6ecee0d730f440c2bf4ebd2
KatsuoSales <84033287+KatsuoSales@users.noreply.github.com>
2021-05-12 01:08:23 +0800

    tostr.py
    使用 str() 轉換函數強制將整數、浮點數與布林值轉換成整數

---
+++ tostr.py
@@ -0,0 +1,53 @@
+# 檔案名稱： tostr.py
+# 檔案說明： 強制轉換成字串
+# -----------------------------
+
+# 設定整數變數與變數值
+int1 = 3
+int2 = 0
+int3 = -4
+
+# 設定浮點數變數與變數值
+flt1 = 3.2
+flt2 = 3.9
+flt3 = 0.0
+flt4 = -4.3
+flt5 = -4.9
+
+# 設定布林值變數與變數值
+bool1 = True
+bool2 = False
+
+
+# 將整數轉換成字串
+int2str1 = str(int1)    # int1 = 3
+int2str2 = str(int2)    # int2 = 0
+int2str3 = str(int3)    # int3 = -4
+
+# 將浮點數轉換成字串
+flt2str1 = str(flt1)      # flt1 = 3.2
+flt2str2 = str(flt2)      # flt2 = 3.9
+flt2str3 = str(flt3)      # flt3 = 0.0
+flt2str4 = str(flt4)      # flt4 = -4.3
+flt2str5 = str(flt5)      # flt5 = -4.9
+
+# 將布林值轉換成字串
+bool2str1 = str(bool1)    # bool1 = True
+bool2str2 = str(bool2)    # bool2 = False
+
+
+#  顯示整數轉換成字串結果
+print(int2str1)
+print(int2str2)
+print(int2str3)
+
+#  顯示浮點數轉換成字串結果
+print(flt2str1)
+print(flt2str2)
+print(flt2str3)
+print(flt2str4)
+print(flt2str5)
+
+#  顯示布林值轉換成字串結果
+print(bool2str1)
+print(bool2str2)
