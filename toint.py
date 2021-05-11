bbe440ae4ef7fd781341b525cb6ba582d33310ed
KatsuoSales <84033287+KatsuoSales@users.noreply.github.com>
2021-05-12 00:59:42 +0800

    toint.py
    使用 int() 轉換函數強制將浮點數、布林值與字串轉換成整數

---
+++ toint.py
@@ -0,0 +1,65 @@
+# 檔案名稱： toint.py
+# 檔案說明： 強制轉換成整數
+# -----------------------------
+
+# 設定浮點數變數與變數值
+flt1 = 3.2
+flt2 = 3.9
+flt3 = 0.0
+flt4 = -4.3
+flt5 = -4.9
+
+# 設定布林值變數與變數值
+bool1 = True
+bool2 = False
+
+# 設定字串變數與變數值
+str1 = "4"
+str2 = "3.2"
+str3 = "0"
+str4 = "0.0"
+str5 = "-3"
+str6 = "-5.2"
+str7 = "Happy"
+
+
+# 將浮點數轉換成整數
+flt2int1 = int(flt1)    # flt1 = 3.2
+flt2int2 = int(flt2)    # flt2 = 3.9
+flt2int3 = int(flt3)    # flt3 = 0.0
+flt2int4 = int(flt4)    # flt4 = -4.3
+flt2int5 = int(flt5)    # flt5 = -4.9
+
+# 將布林值轉換成整數
+bool2int1 = int(bool1)  # bool1 = True
+bool2int2 = int(bool2)  # bool2 = False
+
+# 將字串轉換成整數
+str2int1 = int(str1)    # str1 = "4"
+str2int2 = int(str2)    # str2 = "3.2"
+str2int3 = int(str3)    # str3 = "0"
+str2int4 = int(str4)    # str4 = "0.0"
+str2int5 = int(str5)    # str5 = "-3"
+str2int6 = int(str6)    # str6 = "-5.2"
+str2int7 = int(str7)    # str7 = "Happy"
+
+
+#  顯示浮點數轉換成整數結果
+print(flt2int1)
+print(flt2int2)
+print(flt2int3)
+print(flt2int4)
+print(flt2int5)
+
+#  顯示布林值轉換成整數結果
+print(bool2int1)
+print(bool2int2)
+
+#  顯示字串轉換成整數結果
+print(str2int1)
+print(str2int2)
+print(str2int3)
+print(str2int4)
+print(str2int5)
+print(str2int6)
+print(str2int7)
