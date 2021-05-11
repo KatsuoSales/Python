dc3c0ef8d4cd7bfee8f4ea02733dc02c4b27ccdd
KatsuoSales <84033287+KatsuoSales@users.noreply.github.com>
2021-05-12 01:03:17 +0800

    toflt.py
    使用 float() 轉換函數強制將整數、布林值與字串轉換成整數

---
+++ toflt.py
@@ -0,0 +1,59 @@
+# 檔案名稱： toflt.py
+# 檔案說明： 強制轉換成浮點數
+# -----------------------------
+
+# 設定整數變數與變數值
+int1 = 3
+int2 = 0
+int3 = -4
+
+# 設定布林值變數與變數值
+bool1 = True
+bool2 = False
+
+# 設定字串變數與變數值
+str1 = "4"
+str2 = "3.2"
+str3 = "0"
+str4 = "0.0"
+str5 = "-3"
+str6 = "-5.2"
+str7 = "Happy"
+
+
+# 將整數轉換成浮點數
+int2flt1 = float(int1)    # int1 = 3
+int2flt2 = float(int2)    # int2 = 0
+int2flt3 = float(int3)    # int3 = -4
+
+# 將布林值轉換成浮點數
+bool2flt1 = float(bool1)  # bool1 = True
+bool2flt2 = float(bool2)  # bool2 = False
+
+# 將字串轉換成浮點數
+str2flt1 = float(str1)    # str1 = "4"
+str2flt2 = float(str2)    # str2 = "3.2"
+str2flt3 = float(str3)    # str3 = "0"
+str2flt4 = float(str4)    # str4 = "0.0"
+str2flt5 = float(str5)    # str5 = "-3"
+str2flt6 = float(str6)    # str6 = "-5.2"
+str2flt7 = float(str7)    # str7 = "Happy"
+
+
+#  顯示整數轉換成浮點數結果
+print(int2flt1)
+print(int2flt2)
+print(int2flt3)
+
+#  顯示布林值轉換成浮點數結果
+print(bool2flt1)
+print(bool2flt2)
+
+#  顯示字串轉換成浮點數結果
+print(str2flt1)
+print(str2flt2)
+print(str2flt3)
+print(str2flt4)
+print(str2flt5)
+print(str2flt6)
+print(str2flt7)
